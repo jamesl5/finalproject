775e82afe6cd7e195473aae91d25d46e43819836
Katrina Ezis <katrinaezis@gmail.com>
2015-12-07 02:46:09 -0800

    init

--- js/app.js
+++ js/app.js
@@ -112,7 +112,22 @@ myApp.controller('HomeController', function($scope, $firebaseAuth, $firebaseArra
 
 
 myApp.controller('DashboardController', function($scope) {
+	$scope.getStyle = function(){
+        var transform = ($scope.isSemi ? '' : 'translateY(-50%) ') + 'translateX(-50%)';
 
+        return {
+            'top': $scope.isSemi ? 'auto' : '50%',
+            'bottom': $scope.isSemi ? '5%' : 'auto',
+            'left': '50%',
+            'transform': transform,
+            '-moz-transform': transform,
+            '-webkit-transform': transform,
+            'font-size': $scope.radius/3.5 + 'px'
+        };
+    };
+
+    $scope.curr = 55;
+    
 });
 
 
