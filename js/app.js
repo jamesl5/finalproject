ea1c3edbf7f5ab246daf40175b9a60abd184f1d4
Katrina Ezis <katrinaezis@gmail.com>
2015-12-09 04:40:33 -0800

    added lotsa shit

--- js/app.js
+++ js/app.js
@@ -267,9 +267,29 @@ myApp.controller('HomeController', function($scope, $firebaseAuth, $firebaseArra
 });
 
 myApp.controller('DashboardController', function($scope, $firebase, $firebaseAuth, $firebaseArray, $firebaseObject, $location, $anchorScroll) {
-
 	getStyleFun($scope);
-  	angular.element('.tooltipped').tooltip({delay: 50});
+
+	$('.dropdown-button').dropdown({
+			inDuration: 300,
+			outDuration: 225,
+			constrain_width: true, // Does not change width of dropdown to that of the activator
+			hover: true, // Activate on hover
+			belowOrigin: true
+		// gutter: 50% // Spacing from edge
+		// Displays dropdown below the button
+		// alignment: 'left' // Displays dropdown with edge aligned to the left of button
+		}
+	);
+
+  	$scope.droplet = function(userbadge) {
+  		var badgePopover = new Drop ({
+  			target: document.querySelector(".a" + userbadge['$id']),
+  			content: userbadge['$value'],
+  			position: "bottom center",
+  			openOn: 'click'
+  		});
+  	}
+
 
 	// GETTING BADGES
 	$scope.Math = window.Math;
